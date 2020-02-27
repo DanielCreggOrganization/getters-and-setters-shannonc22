36bd30f778cc9c2f9c7c50df9d6aa4b30246a65b
shannonc22 <g00358845@gmit.ie>
2020-02-27 14:34:41 +0000

    class

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
 public class Main {
     // Main method
+    public static void main(String[] args) {
+        
+    Person personObject1 = new Person();
+
+    personObject1.setFirstName("Joe");
+    System.out.println(personObject1.getFirstName());
+
+    personObject1.setMiddleNames("Bob John");
+    System.out.println(personObject1.getMiddleNames());
+
+    personObject1.setUserName("abcdefghijklmnopqrstuvwxyz");
+    System.out.println(personObject1.getUserName());
+    
+    }
+
+    
 }
--- Person.java
+++ Person.java
@@ -1,5 +1,48 @@
 class Person {
     // Instance Variables
-    
+    private String firstName;
+    private String[] middleNames;
+    private String userName;
+
     // Methods
+
+    
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleNames() {
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < middleNames.length; i++){
+            sb.append(middleNames[i] + " ");
+        }
+        String middleNames = sb.toString();
+        return middleNames;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+
+    public void setMiddleNames(String middleNames){
+        this.middleNames = middleNames.split(" ");
+    }
+
+    public void setUserName(String userName){
+        if (userName.length() > 10){
+                    this.userName = userName.substring(0, 10);
+        }else {
+                    this.userName = userName;
+        }
+    }
+
+        
+    
+
+
+
 }
